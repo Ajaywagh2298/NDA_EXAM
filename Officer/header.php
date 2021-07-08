b07e2699c4c37e15d512a6b536423cd43e92e382
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2021-07-08 12:37:47 +0530

    commit

--- Officer/header.php
+++ Officer/header.php
@@ -133,8 +133,7 @@
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
                     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="./">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page"></li>
+
                     </ol>
                 </div>
             </div>
