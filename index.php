b25c3ad33951c6f84afad75a3b4f4bea47af043a
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2021-08-22 19:58:02 +0530

    commit

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
         </div>
     </div>
 </div>
-
+<!-- helli -->
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
